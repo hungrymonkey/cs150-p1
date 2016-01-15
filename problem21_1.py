34f6451e1b342383419301c37b93d35b5702e9f8
Ted L Chang <tlc008@ieng6-202.ucsd.edu>
2016-01-14 19:18:04 -0800

    maybe I am done with problem 21_1

--- problem21_1.py
+++ problem21_1.py
@@ -1,4 +1,6 @@
 """ Pylint told me I should add an doc String"""
+import csv
+import sys
 
 def sanitize_input( var_i ):
     if all( 0 <= i <= 1 for i in var_i[:-1]) is False:
@@ -7,8 +9,21 @@ def sanitize_input( var_i ):
 
 
 def check_config( var_i ):
-    inital_config = var_i(len(var_i)-1)
+    inital_config = var_i[len(var_i)-1]
     if len(var_i) - 1 <= inital_config:
        return False
     return sum( var_i[:-1]) == 0
 
+def main():
+    l = []
+    for line in csv.reader(sys.stdin):
+        l.append(map( lambda x: int(x), line ))
+    if sanitize_input(l[0]) is False:
+        print "invalid input"
+        sys.exit(0)
+    print check_config(l[0])
+
+
+if __name__ == "__main__":
+    main()
+    
