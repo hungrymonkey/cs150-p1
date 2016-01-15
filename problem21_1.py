eb94f4bb6f172cb71931028868a82a547bb58dd3
Ted L Chang <tlc008@acs-cseb240-10.ucsd.edu>
2016-01-15 15:54:50 -0800

    change output based on TAs

--- problem21_1.py
+++ problem21_1.py
@@ -5,13 +5,13 @@ import sys
 def sanitize_input( var_i ):
     if all( 0 <= i <= 1 for i in var_i[:-1]) is False:
        return False
+    inital_config = var_i[len(var_i)-1]
+    if len(var_i) - 1 <= inital_config or inital_config < 0:
+       return False
     return True
 
 
 def check_config( var_i ):
-    inital_config = var_i[len(var_i)-1]
-    if len(var_i) - 1 <= inital_config:
-       return False
     return sum( var_i[:-1]) == 0
 
 def main():
