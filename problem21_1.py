8d5ee1c3369183e6ec431a8429bc6cc131ea5fe2
Ted L Chang <tlc008@ieng6-202.ucsd.edu>
2016-01-14 18:59:52 -0800

    need to add stdin

--- problem21_1.py
+++ problem21_1.py
@@ -1,8 +1,14 @@
 """ Pylint told me I should add an doc String"""
 
 def sanitize_input( var_i ):
+    if all( 0 <= i <= 1 for i in var_i[:-1]) is False:
+       return False
+    return True
+
+
+def check_config( var_i ):
     inital_config = var_i(len(var_i)-1)
     if len(var_i) - 1 <= inital_config:
        return False
-    if all( 0 <= i <= 1 for i in var_i[:-1]) is False:
-       return False
+    return sum( var_i[:-1]) == 0
+
