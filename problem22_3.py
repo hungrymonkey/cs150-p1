f3c4a42449c24b5c8ad485ff9a014d896cc4a580
Ted L Chang <tlc008@acs-cseb220-09.ucsd.edu>
2016-01-21 21:03:45 -0800

    added problem22 files

---
+++ problem22_3.py
@@ -0,0 +1,122 @@
+""" Pylint told me I should add an doc String"""
+import csv
+import sys
+import copy
+
+
+MOVES = ['L','R','S']
+
+def find_adjacent( var_frontier ):
+    var_moves = []
+    for var_m in MOVES:
+        var_v = valid_move( var_frontier, var_m)
+        if var_v is not ():
+           var_moves.append( var_v)
+    return var_moves
+
+def valid_move( var_frontier , var_m):
+    if var_m == 'L':
+        return left(var_frontier)
+    elif var_m == 'R':
+        return right(var_frontier)
+    elif var_m == 'S':
+        return suck(var_frontier)
+    
+def check_config( var_i ):
+    return sum( var_i[:-1]) == 0
+
+def left( var_frontier ):
+    var_config, var_move = var_frontier[-1]
+    var_f = [ f for f, m in var_frontier ]
+    var_c = copy.copy(var_config)
+    var_c[-1] -= 1 
+    if inBounds( var_c ):
+       if var_c not in var_f:
+            return ( var_c, 'L' )
+    return ()
+
+def right( var_frontier ):
+    var_config, var_move = var_frontier[-1]
+    var_f = [ f for f, m in var_frontier ]
+    var_c = copy.copy(var_config)
+    var_c[-1] += 1 
+    if inBounds( var_c ):
+        if var_c not in var_f:
+             return ( var_c, 'R')
+    return ()
+
+def suck( var_frontier ):
+    var_config, var_move = var_frontier[-1]
+    var_c = copy.copy(var_config)
+    if var_c[var_c[-1]] is 1:
+        var_c[var_c[-1]] -= 1
+        return (var_c, 'S')
+    else:
+        return ()
+
+def inBounds( var_c ):
+    return var_c[-1] in range(0, len(var_c)-1)
+    
+
+def sanitize_input( var_i ):
+    if inBounds( var_i ) is False:
+       return False
+    if all( 0 <= i <= 1 for i in var_i[:-1]) is False:
+       return False
+    return True
+
+def find_path( var_c ):
+    return dfs( var_c )
+
+
+def dfs( var_c ):
+    stack = []
+    path = []
+    stack.append( (list(var_c), "" ) )
+    while stack:
+        node_c, node_m = stack.pop()
+        path.append( (node_c, node_m))
+        if check_config(node_c):
+            return [ m for _, m in path ]
+        adj =  find_adjacent( path )
+        if not adj:
+            path.pop()
+        else:
+            for a in reversed(adj):
+                stack.append(  a )
+    return []
+
+
+def bfs( var_c ):
+    queue = []
+    queue.append( [(list(var_c), "" ) ])
+    while queue:
+        path = queue.pop()
+        node_c, node_m = path[-1]
+        if check_config(node_c):
+            return [ m for _, m in path ]
+        adj =  find_adjacent( path )
+        if not adj and not queue:
+            return []
+        else:
+            for a in adj:
+                p = list( path )
+                p.append( a )
+                queue.insert(0,  p )
+
+
+
+def main():
+    var_l = []
+    for line in csv.reader(sys.stdin):
+        var_l.append(map( lambda x: int(x), line ))
+    if sanitize_input(var_l[0]) is False:
+        print "invalid input"
+        sys.exit(0)
+    print "".join(find_path( var_l[0] ))
+    
+
+
+if __name__ == "__main__":
+    main()
+    
--- problem22_6.py
+++ problem22_6.py
@@ -1,67 +1,77 @@
 """ Pylint told me I should add an doc String"""
-import Queue
 import csv
 import sys
 import copy
-import collections
-import operator
-MOVES = {'L': [0,-1],'U': [-1,0],'R': [0,1],'D': [1,0],'S': [0,0]}
-MOVE = ['L','U','R','D','S']
-DEPTH = 7
+import Queue
 
-""" This function access a list of config + moves"""
-""" Example [( [[0,0],[0,0],[1,1]],'L')] """
+
+MOVES = ['L','R','S']
+
 def find_adjacent( var_frontier ):
     var_moves = []
-    for var_m in MOVE:
+    for var_m in MOVES:
         var_v = valid_move( var_frontier, var_m)
         if var_v is not ():
            var_moves.append( var_v)
     return var_moves
 
 def valid_move( var_frontier , var_m):
-    if var_m == 'S':
+    if var_m == 'L':
+        return left(var_frontier)
+    elif var_m == 'R':
+        return right(var_frontier)
+    elif var_m == 'S':
         return suck(var_frontier)
-    else:
-        return move(var_frontier, var_m)
+
 def heuristic_cost( var_i ):
-    return  sum( map( sum, var_i[:-1])) 
+    return sum( var_i[:-1])
     
 def check_config( var_i ):
-    return sum( map( sum, var_i[:-1])) == 0
+    return sum( var_i[:-1]) == 0
 
-def move( var_frontier, var_m ):
+
+def left( var_frontier ):
     var_config, var_move = var_frontier[-1]
     var_f = [ f for f, m in var_frontier ]
-    var_c = copy.deepcopy(var_config)
-    var_c[-1][0] += MOVES[var_m][0]
-    var_c[-1][1] += MOVES[var_m][1]
+    var_c = copy.copy(var_config)
+    var_c[-1] -= 1 
     if inBounds( var_c ):
        if var_c not in var_f:
-            return ( var_c, var_m )
+            return ( var_c, 'L' )
     return ()
 
+def right( var_frontier ):
+    var_config, var_move = var_frontier[-1]
+    var_f = [ f for f, m in var_frontier ]
+    var_c = copy.copy(var_config)
+    var_c[-1] += 1 
+    if inBounds( var_c ):
+        if var_c not in var_f:
+             return ( var_c, 'R')
+    return ()
 
 def suck( var_frontier ):
     var_config, var_move = var_frontier[-1]
-    var_c = copy.deepcopy(var_config)
-    if var_c[var_c[-1][0]][var_c[-1][1]] is 1:
-        var_c[var_c[-1][0]][var_c[-1][1]] -= 1
+    var_c = copy.copy(var_config)
+    if var_c[var_c[-1]] is 1:
+        var_c[var_c[-1]] -= 1
         return (var_c, 'S')
     else:
         return ()
 
 def inBounds( var_c ):
-    coord = var_c[-1]
-    return coord[0] in range(0, len(var_c)-1) and coord[1] in range(0, len(var_c[0]))
+    return var_c[-1] in range(0, len(var_c)-1)
     
 
 def sanitize_input( var_i ):
     if inBounds( var_i ) is False:
        return False
-    if all( map( lambda l: all( 0 <= i <= 1 for i in l), var_i[:-1] ) ) is False:
+    if all( 0 <= i <= 1 for i in var_i[:-1]) is False:
        return False
     return True
+
+def find_path( var_c ):
+    return a_star( var_c )
 
 def reconstruct_path( var_i, came_from ):
     path = []
@@ -97,14 +107,30 @@ def a_star( var_c ):
                came_from[str(c)] = config
     return []
        
+    
+    
 
+def dfs( var_c ):
+    stack = []
+    path = []
+    stack.append( (list(var_c), "" ) )
+    while stack:
+        node_c, node_m = stack.pop()
+        path.append( (node_c, node_m))
+        if check_config(node_c):
+            return [ m for _, m in path ]
+        adj =  find_adjacent( path )
+        if not adj:
+            path.pop()
+        else:
+            for a in reversed(adj):
+                stack.append(  a )
+    return []
 
-def find_path( var_c ):
-    return a_star( var_c )
 
 def bfs( var_c ):
     queue = []
-    queue.append( [(copy.deepcopy(var_c), "" ) ])
+    queue.append( [(list(var_c), "" ) ])
     while queue:
         path = queue.pop()
         node_c, node_m = path[-1]
@@ -117,34 +143,18 @@ def bfs( var_c ):
             for a in adj:
                 p = list( path )
                 p.append( a )
-                queue.insert( 0, p )
+                queue.insert(0,  p )
 
-def dls( var_c, depth ):
-    stack = []
-    path = []
-    stack.append( (copy.deepcopy(var_c), "" ) )
-    while stack:
-        node_c, node_m = stack.pop()
-        path.append( (node_c, node_m))
-        if check_config(node_c):
-            return [ m for _, m in path ]
-        adj = find_adjacent( path )
-        if not adj or len(path) == depth:
-            path.pop()
-        else:
-            for a in reversed(adj):
-                stack.append(  a )
-    return []
 
 
 def main():
     var_l = []
     for line in csv.reader(sys.stdin):
         var_l.append(map( lambda x: int(x), line ))
-    if sanitize_input(var_l) is False:
+    if sanitize_input(var_l[0]) is False:
         print "invalid input"
         sys.exit(0)
-    print "".join(find_path( var_l ))
+    print "".join(find_path( var_l[0] ))
     
 
 
